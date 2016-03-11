2e22fc590a2ae5f1a389b93a46fa42c49cc8b348
Karan Jaiswal <karanjaiswal632@gmail.com>
2016-03-11 04:34:23 +0100

    Added files via upload

---
+++ Conditional/HelloObject.java
@@ -0,0 +1,20 @@
+public class HelloObject{
+	public static void main(String args[]){
+
+		System.out.println("Hello World");
+
+		Nepal visa=new Nepal();
+		visa.getDispaly();
+		visa.getEducation();
+	}
+
+}
+	class Nepal{
+		public void getDispaly(){
+			System.out.println("I am in Nepal nowadays");
+
+			}
+			public void getEducation(){
+				System.out.println("I got degree in Nepal");
+		}
+	}
